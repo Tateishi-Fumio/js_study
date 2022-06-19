9fa5b000573930999c3da055cef133e8981ce876
Tateishi-Fumio <80140202+Tateishi-Fumio@users.noreply.github.com>
2022-06-20 00:22:49 +0900

    add: テキスト追加

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-const num = 1300;
+import "./styles.css";
 
-console.log(num.toLocaleString());
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、入力値を削除する
+  const inputText = document.getElementById("add-text").value;
+
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグに子要素追加
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
